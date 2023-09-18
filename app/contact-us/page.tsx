82a63edc276ac85e5777516abb460a297efd2ab4
Muhammad Hamza <hamzaansaree15@gmail.com>
2023-09-19 02:20:29 +0500

    Make Navigation Responsive

---
+++ app/contact-us/page.tsx
@@ -0,0 +1,8 @@
+const ContactUs: React.FC = () => {
+    return(
+        <h1>Contact us</h1>
+    )
+}
+
+
+export default ContactUs;
--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,7 +1,10 @@
-export default () => {
+const Menu:React.FC = () => {
     return(
         <h1 className="w-screen h-full">
             Menu Page
         </h1>
     )
 }
+
+
+export default Menu;
